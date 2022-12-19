70502d661e1bde8f229ecbf0a1aea72f4d3e6e21
denryh <dtdat101101@gmail.com>
2022-12-20 03:05:38 +0700

    added back btn

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,7 +1,17 @@
+import { useRouter } from "next/router";
+
 const NavBar = () => {
+  const router = useRouter();
+
   return (
-    <nav className="font-serif grid place-items-center bg-orange-200 p-4 font-display text-3xl text-orange-800">
-      <span>xigonfood</span>
+    <nav className="font-serif relative grid place-items-center bg-orange-200 p-6 text-orange-800">
+      <span
+        onClick={() => router.back()}
+        className="absolute top-8 left-4 cursor-pointer rounded-full border-2 border-orange-800 px-4 hover:bg-white"
+      >
+        &lt;
+      </span>
+      <span className="font-display text-3xl">xigonfood</span>
     </nav>
   );
 };
